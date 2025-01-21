c0d9753e1ce3af8270b7a902e119787c09f33f9d
majuss <biomajuss@gmail.com>
2025-01-21 09:37:59 +0100

    fixing imports

--- components/enocean/sensor/enocean_sensor.h
+++ components/enocean/sensor/enocean_sensor.h
@@ -2,8 +2,8 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/enocean/enocean.h"
-// #include "esphome/components/sensor/sensor.h"
-#include "esphome.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/esphome.h"
 
 namespace esphome::enocean {
 
