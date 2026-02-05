41528c8b822f4533b05018efbd8ab5e47315a0fd
majuss <biomajuss@gmail.com>
2026-02-05 19:46:59 +0100

    Version bump

--- components/enocean/__init__.py
+++ components/enocean/__init__.py
@@ -25,4 +25,4 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
-    cg.add_library("majuss/EnOcean", "1.1.3")
+    cg.add_library("majuss/EnOcean", "1.1.4")
